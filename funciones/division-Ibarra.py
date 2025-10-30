eff2602379aca1d09016df0a50d3fdb695cb0304
MineIsTheRage <162656317+MineIsTheRage@users.noreply.github.com>
2025-10-30 17:41:38 -0300

    correccion de Division-Ibarra.py a division-Ibarra.py

--- funciones/division-Ibarra.py
+++ funciones/division-Ibarra.py
@@ -4,8 +4,3 @@ def dividir(a, b):
  if b == 0:
   return None
  return a / b
-#tests/test_dividir.py
-from funciones.dividir import dividir
-def test_dividir():
- assert dividir(10, 2) == 5
- assert dividir(5, 0) is None
