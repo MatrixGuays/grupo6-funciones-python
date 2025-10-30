75b1cc4a3503157acea1917dac244540afc2220d
Ezequiel-Rodrigo <rodriezequiel234@gmail.com>
2025-10-30 15:18:29 -0300

    correcion de archivos

--- funciones/divisiones_rodrigo_sanchez.py
+++ funciones/divisiones_rodrigo_sanchez.py
@@ -3,8 +3,3 @@ def dividir(a, b):
  if b == 0:
   return None
  return a / b
-#tests/test_dividir.py
-from funciones.dividir import dividir # type: ignore
-def test_dividir():
- assert dividir(10, 2) == 5
- assert dividir(5, 0) is None
--- funciones/multiplicaciones_rodrigo_sanchez.py
+++ funciones/multiplicaciones_rodrigo_sanchez.py
@@ -1,8 +1,3 @@
 def multiplicar(a, b):
  """Devuelve el producto de dos números."""
  return a * b
-#tests/test_multiplicar.py
-from funciones.multiplicar import multiplicar # type: ignore
-def test_multiplicar():
- assert multiplicar(3, 4) == 12
- assert multiplicar(-2, 5) == -10
